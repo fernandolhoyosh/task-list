4703fb349d55f13f8929724ac28ac590a043c1b9
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-06-04 22:37:24 -0500

    add function add task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,10 +1,12 @@
+import { IoMdAddCircle } from "react-icons/io";
+
 import '../index.css'
 
 function AddTask(){
     return (
         <div className="add-task">
             <input type="text" placeholder="Add your new task ..." />
-            <button>+</button>
+            <button><IoMdAddCircle/></button>
         </div>
     )
 
