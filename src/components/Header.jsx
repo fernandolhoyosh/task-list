0d15cf8e4cf348089d85aa88b935893d4bb48fca
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-06-29 22:33:27 -0500

    Add text header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,23 @@ import "../index.css";
 
 function Header() {
   return (
-    <header>
-      <h2>Task List</h2>
-    </header>
+    <>
+      <header>
+        <h2>Task List</h2>
+      </header>
+      <div className="text-header">
+        <p>
+          Welcome to your to-do list app where you can organize your to-do
+          activities, you can add, update or delete tasks as well as check
+          completed tasks.
+        </p>
+        <p>
+          <strong>Note: </strong>
+          The add a description field is not mandatory, if you add it you can see
+          the description of the task by hovering the cursor over the task.
+        </p>
+      </div>
+    </>
   );
 }
 
