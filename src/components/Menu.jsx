0979f18457f52d20985393422cbea5fa4216f0f7
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-07-03 23:12:19 -0500

    Add styles a Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import { AiFillHome } from 'react-icons/ai';
+import { FaTasks } from 'react-icons/fa';
+import { BsInfoSquareFill } from 'react-icons/bs';
 import "./Menu.css"
 
 const Menu = () => {
     return (
         <nav className="menu-nav">
-            <Link to="/">Home</Link>
-            <Link to="/task-list">Task list</Link>
-            <Link to="/about-us">About us</Link>
+            <Link to="/" className="nav-item"><AiFillHome className="nav-icons"/>Home</Link>
+            <Link to="/task-list" className="nav-item"><FaTasks className="nav-icons"/>Task list</Link>
+            <Link to="/about-us" className="nav-item"><BsInfoSquareFill className="nav-icons"/>About us</Link>
         </nav>
     )
 }
