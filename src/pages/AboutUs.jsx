daa67317a922a57f483708a1182dc7931940aa14
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-07-04 00:24:08 -0500

    Add content AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,25 @@
+import "./AboutUs.css";
+
 const AboutUs = () => {
-    return <h3>About Us</h3>
-}
+  return (
+    <section className="about-section">
+      <h3>Funcionalidades actuales:</h3>
+
+      <ul>
+        <li>Agregar tareas</li>
+        <li>Añadir una descripción a una tarea (No obligatorio)</li>
+        <li>Validación del formulario</li>
+        <li>Marcar una tarea como terminada o pendiente</li>
+        <li>Botón eliminar tareas de la lista</li>
+        <li>Guardar y leer las tareas y su estado en el localStorage</li>
+        <li>Actualizar el nombre de una tarea</li>
+        <li>Eliminar una tarea especifica</li>
+        <li>Contador de tareas con estado pendiente</li>
+        <li>Implementación de React Router y Lazy</li>
+        <li>Menu de navegación</li>
+      </ul>
+    </section>
+  );
+};
 
 export default AboutUs;
