f89a3e96a1eb3c039f14122017be3915fae66444
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-07-18 23:56:44 -0500

    Update styles menu nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,12 +10,13 @@ const Menu = () => {
 
     const {colorMode, toggleColorMode} = useColorMode();
     const TagMenuVariant = colorMode === "dark" ? "solid" : "subtle";
-
+    const classLinkNav = colorMode === "dark" ? "nav-item-dark" : "nav-item-light";
+    
     return (
         <Tag className="menu-nav" size={"lg"} borderRadius={"none"} variant={TagMenuVariant} colorScheme="gray" >
-            <Link to="/" className="nav-item"><AiFillHome className="nav-icons"/>Home</Link>
-            <Link to="/task-list" className="nav-item"><FaTasks className="nav-icons"/>Tasks</Link>
-            <Link to="/about-us" className="nav-item"><BsInfoSquareFill className="nav-icons"/>About us</Link>
+            <Link to="/" className={classLinkNav}><AiFillHome className="nav-icons"/>Home</Link>
+            <Link to="/task-list" className={classLinkNav}><FaTasks className="nav-icons"/>Tasks</Link>
+            <Link to="/about-us" className={classLinkNav}><BsInfoSquareFill className="nav-icons"/>About us</Link>
             <button className="theme-button" onClick={toggleColorMode}>{colorMode === "dark" ? <BsSunFill/>:<BsMoonFill/>}</button>
         </Tag>
     )
